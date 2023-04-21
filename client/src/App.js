86cfe490ce8e6c54d13671af8edc7750b93c0289
Danielle Thoreson <danielle_thoreson@ymail.com>
2023-04-21 12:12:44 -0700

    initialize client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
-
-require('./server/routes/????.routes')(app);
+require('./config/mongoose.config'); // This is new
+app.use(cors());
+app.use(express.json()); // This is new
+app.use(express.urlencoded({ extended: true })); // This is new
+require('./routes/product.routes')(app);
+    
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
