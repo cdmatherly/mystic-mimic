4cf6de7e333c1b7d3f6ba31503554758be8683aa
Danielle Thoreson <danielle_thoreson@ymail.com>
2023-04-21 16:44:53 -0700

    More corrections to login/reg

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route, Link, Navigate } from 'react-router-dom';
-import Register from './views/Register';
-import Login from './views/Login';
+import Register from './views/RegisterView';
+import Login from './views/LoginView';
 
 function App() {
   return (
--- client/src/views/LoginView.jsx
+++ client/src/views/LoginView.jsx
@@ -2,12 +2,13 @@ const Login = (props) => {
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Mystic</span> Mimic</h1>
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
+                    {/* <img
                         className="mx-auto h-10 w-auto"
                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                         alt="Your Company"
-                    />
+                    /> */}
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                         Sign in to your account
                     </h2>
@@ -16,9 +17,11 @@ const Login = (props) => {
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6" action="#" method="POST">
                         <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                Email address
-                            </label>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Email address
+                                </label>
+                            </div>
                             <div className="mt-2">
                                 <input
                                     id="email"
@@ -36,11 +39,6 @@ const Login = (props) => {
                                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                                     Password
                                 </label>
-                                {/* <div className="text-sm">
-                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                                        Forgot password?
-                                    </a>
-                                </div> */}
                             </div>
                             <div className="mt-2">
                                 <input
