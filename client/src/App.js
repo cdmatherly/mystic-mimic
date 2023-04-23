819472c940fde4d5f44a7bf75c54e3b42c41edd9
Danielle Thoreson <danielle_thoreson@ymail.com>
2023-04-23 01:42:11 -0700

    More updates to dash and cac

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Link, Navigate } from 'react-router-dom';
 import Register from './views/RegisterView';
 import Login from './views/LoginView';
 import Dash from './views/DashView';
+import CreateACharacter from './views/CreateACharacterView';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route element={<Register />} path='/register' />
         <Route element={<Login />} path='/login' />
         <Route element={<Dash />} path='/dash' />
+        <Route element={<CreateACharacter />} path='/cac' />
       </Routes>
     </div>
   );
