cb6d01400a052e02cb678aef9e469b58f0b5feb1
Danielle Thoreson <danielle_thoreson@ymail.com>
2023-04-24 14:24:02 -0700

    Made correction to Link

--- client/src/views/LandingPageView.jsx
+++ client/src/views/LandingPageView.jsx
@@ -19,7 +19,7 @@ const LandingPage = (props) => {
                                     <h3 className="text-5xl  tracking-tight leading-tight mb-12">
                                         Are you ready <br /><span>for an adventure?</span>
                                     </h3>
-                                    <Link path='/register'>
+                                    <Link to='/register'>
                                         <button type="button"
                                             className="inline-block px-7 py-3 border-2 border-white text-white font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
                                             data-mdb-ripple="true" data-mdb-ripple-color="light">
