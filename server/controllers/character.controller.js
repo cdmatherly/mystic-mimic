d01b594dd4745961e10b6c1dcc90d83babd305fc
AChang0402 <alexchang.0402@gmail.com>
2023-04-24 17:45:17 -0700

    added deleteCharacterAndUpdateUser

--- server/controllers/character.controller.js
+++ server/controllers/character.controller.js
@@ -1,7 +1,7 @@
 const { Character } = require("../models/character.model")
 const { User } = require("../models/user.model")
 
-// Get character by ID
+// get character by ID
 module.exports.getCharacterById = (req, res) => {
     Character.findById(req.params.id)
         .then((character) => {
@@ -12,7 +12,7 @@ module.exports.getCharacterById = (req, res) => {
         })
 }
 
-// Get all characters
+// get all characters
 module.exports.getAllCharacters = (req, res) => {
     Character.find()
         .then((characters) => {
@@ -23,19 +23,19 @@ module.exports.getAllCharacters = (req, res) => {
         })
 }
 
-// Get Characters by User
+// Get characters by user
 module.exports.getCharactersByUser = (req, res) => {
     User.findById(req.params.user)
         .then((user) => {
             let userCharacters = user.characters
-            Character.find({_id: {$in: userCharacters}})
-            .then((characters)=>{
-                console.log(characters)
-                return res.json(characters)
-            })
-            .catch((err)=>{
-                return res.status(400).json(err)
-            })
+            Character.find({ _id: { $in: userCharacters } })
+                .then((characters) => {
+                    console.log(characters)
+                    return res.json(characters)
+                })
+                .catch((err) => {
+                    return res.status(400).json(err)
+                })
             // return res.json(user)
         })
         .catch((err) => {
@@ -64,7 +64,7 @@ module.exports.createCharacterAndUpdateUser = (req, res) => {
             User.findById(req.params.user)
                 .then((user) => {
                     console.log(user.characters)
-                    let updateCharacters = { "characters": [...user.characters, character._id]}
+                    let updateCharacters = { "characters": [...user.characters, character._id.toString()] }
                     console.log(updateCharacters)
                     User.findByIdAndUpdate(req.params.user, updateCharacters, { runValidators: true, new: true })
                         .catch((err) => {
@@ -81,7 +81,7 @@ module.exports.createCharacterAndUpdateUser = (req, res) => {
         });
 }
 
-// Edit a Character
+// Edit a character
 module.exports.updateCharacterById = (req, res) => {
     Character.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true })
         .then((character) => {
@@ -92,11 +92,43 @@ module.exports.updateCharacterById = (req, res) => {
         })
 }
 
-// Delete A Character
+// Delete a character
 module.exports.deleteCharacter = (req, res) => {
     Character.findByIdAndDelete(req.params.id)
         .then((character) => {
             return res.json(character)
+        })
+        .catch((err) => {
+            return res.status(400).json(err)
+        })
+}
+
+// Delete a character and update characters in given user document
+module.exports.deleteCharacterAndUpdateUser = (req, res) => {
+    Character.findByIdAndDelete(req.params.id)
+        .then((character) => {
+
+            // Alternate way to find and update in one step but doesn't return user data in response for some reason
+            // User.updateOne( {_id:req.params.user},{ $pull: {characters: req.params.id}},{ runValidators: true, new: true })
+            //     .then(user => {
+            //         return res.json(user)
+            //     })
+            
+            User.findById(req.params.user)
+                .then((user) => {
+                    const filteredCharacters = user.characters.filter((character) => (character !== req.params.id))
+                    let updateCharacters = { "characters": filteredCharacters }
+                    User.findByIdAndUpdate(req.params.user, updateCharacters, { runValidators: true, new: true })
+                        .then((user) => {
+                            return res.json(user)
+                        })
+                        .catch((err) => {
+                            return res.status(400).json(err)
+                        })
+                })
+                .catch((err) => {
+                    return res.status(400).json(err)
+                })
         })
         .catch((err) => {
             return res.status(400).json(err)
