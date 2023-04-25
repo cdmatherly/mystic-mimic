11c8a7b947a7b9a81cdcfd001b015ead059fb6ab
NassarLaith <Nassarlaith.9@gmail.com>
2023-04-25 17:14:59 -0400

    updated createACreateView - laith

--- client/src/components/StatSelect.jsx
+++ client/src/components/StatSelect.jsx
@@ -20,7 +20,7 @@ const StatSelect = (props) => {
                 </label>
             </div>
             <div className="md:w-2/3">
-                <input id={stat} name={stat} type="number" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" min={8} max={15} onChange={ (event) => onChangeHandle(event.target.value) } />
+                <input id={stat} name={stat} type="number" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" min={8} max={15} defaultValue={8} onChange={ (event) => onChangeHandle(event.target.value) } />
             </div>
         </div>
     )
