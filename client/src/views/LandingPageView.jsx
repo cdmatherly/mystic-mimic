48269451c9a1d1c7651783236f676d94e2a7fcbd
Danielle Thoreson <danielle_thoreson@ymail.com>
2023-04-25 10:39:38 -0700

    added to hover effect on landing button

--- client/src/views/LandingPageView.jsx
+++ client/src/views/LandingPageView.jsx
@@ -21,7 +21,7 @@ const LandingPage = (props) => {
                                     </h3>
                                     <Link to='/register'>
                                         <button type="button"
-                                            className="inline-block px-7 py-3 border-2 border-white text-white font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                                            className="inline-block px-7 py-3 border-2 border-white text-white font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-15 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
                                             data-mdb-ripple="true" data-mdb-ripple-color="light">
                                             Get started
                                         </button>
