275e132933d42f2e8bad42123bfa9cc8b08f029b
Chase <chsmatherly@gmail.com>
2023-04-25 14:24:30 -0700

    All campaigns API call

---
+++ client/src/components/DeleteModal.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+export default function Modal(props) {
+    const { character, handleDelete } = props
+    const [showModal, setShowModal] = React.useState(false);
+
+    return (
+        <>
+            <button
+                className="shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="button"
+                onClick={() => setShowModal(true)}
+            >
+                Delete
+            </button>
+            {showModal ? (
+                <>
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                    >
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                    <h3 className="text-3xl font-semibold">
+                                        Delete <span className="text-fuchsia-700 underline">{character.name}</span>
+                                    </h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                            ×
+                                        </span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative p-6 flex-auto">
+                                    <p className="my-4 text-slate-500 text-lg leading-relaxed text-center">
+                                        You are about to delete this character. This cannot be undone.<br/> Are you sure you want to continue?
+                                    </p>
+                                </div>
+                                {/*footer*/}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                    <button
+                                        className="text-gray-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Cancel
+                                    </button>
+                                    <button
+                                        className="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => handleDelete()}
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
+        </>
+    );
+}
--- client/src/components/EachCampaign.jsx
+++ client/src/components/EachCampaign.jsx
@@ -1,6 +1,8 @@
 import charImg from '../images/bard_lute_char_img.jpeg';
 
 const EachCampaign = (props) => {
+    const { campaign } = props
+
     return (
         <>
             <div className="relative">
@@ -10,7 +12,7 @@ const EachCampaign = (props) => {
                             <img className="h-20" src={charImg} alt="Character Image"></img>
                             <div className="mb-6">
                                 <label className="text-gray-500 font-bold">
-                                    Campaign Name
+                                    {campaign.name}
                                 </label>
                             </div>
                             <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">Join Campaign</button>
