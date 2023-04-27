808cc75f26f59a07fc86fd04947d9b64195386b0
Chase <chsmatherly@gmail.com>
2023-04-27 10:32:21 -0700

    added how many characters are in each campaign

--- client/src/components/EachCampaign.jsx
+++ client/src/components/EachCampaign.jsx
@@ -9,16 +9,18 @@ const EachCampaign = (props) => {
     return (
         <>
             <div className="relative">
-                <div className="absolute -inset-4 bg-gradient-to-r from-purple-500 to-sky-400 rounded-lg blur-lg"></div>
-                <div className="relative max-w-full h-full rounded overflow-hidden shadow-lg bg-white">
+                <div className="absolute rounded-lg -inset-4 bg-gradient-to-r from-purple-500 to-sky-400 blur-lg"></div>
+                <div className="relative h-full max-w-full overflow-hidden bg-white rounded shadow-lg">
                         <div className='p-20'>
                             <img className="h-20" src={charImg} alt="Character Image"></img>
                             <div className="mb-6">
-                                <label className="text-gray-500 font-bold">
+                                <p className="font-bold text-gray-500">
                                     {campaign.name}
-                                </label>
+                                </p>
+                                <p className='font-semibold text-gray-400'>{campaign.characters.length} Players</p>
                             </div>
                             <ShowOneCampaignModal campaign={campaign} characters={characters} setNewCampaign={setNewCampaign} setUpdateCampaigns={setUpdateCampaigns}/>
+                            <p className='mt-4 font-bold'>Owner: <span className='font-semibold text-red-400'>{campaign.owner.username}</span></p>
                         </div>
                 </div>
             </div>
