e9d184e3f78423a963c7c65bdf6896ca6911c1fc
Danielle Thoreson <danielle_thoreson@ymail.com>
2023-04-27 15:36:43 -0700

    chat merge

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -34,13 +34,16 @@ const Chat = (props) => {
 
     return (
         <>
-            <div className="justify-between flex flex-col h-80 w-80 bg-white rounded-lg">
-                <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
-                    <div className="relative flex items-center space-x-4 p-1">
-                        <div className="ml-2 mb-5 -mr-4">
-                            <span class="relative flex h-3 w-3">
-                                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                                <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+            <div className="flex flex-col justify-between bg-white rounded-lg h-80 w-80">
+                <div className="flex justify-between py-3 border-b-2 border-gray-200 sm:items-center">
+                    <div className="relative flex items-center p-1 space-x-4">
+                        <div className="relative">
+                            <span className="absolute text-green-500 right-0 bottom-0 left-0.5">
+                                <svg width="20" height="20">
+                                    {isConnected ? <circle cx="8" cy="8" r="8" fill="currentcolor"></circle> :
+                                        <circle cx="8" cy="8" r="8" fill="red"></circle>
+                                    }
+                                </svg>
                             </span>
                         </div>
                         <div className="flex flex-col leading-tight">
@@ -84,15 +87,15 @@ const Chat = (props) => {
                 {/* Button Section */}
                 <div className="px-4 pt-4 mb-2 border-t-2 border-gray-200 sm:mb-0">
                     <div className="relative flex">
-                        <input value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} onKeyDown={(e) => e.key === "Enter" && sendMessage()} type="text" placeholder="Send a message" className="w-full py-3 pl-10 text-gray-600 placeholder-gray-600 bg-gray-200 rounded-md focus:outline-none focus:placeholder-gray-400"/>
-                            <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
-                                <button onClick={sendMessage} type="button" className="inline-flex items-center justify-center px-2 py-3 text-white transition duration-500 ease-in-out bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none">
-                                    <span className="font-bold">Send</span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6 ml-2 transform rotate-90">
-                                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                                    </svg>
-                                </button>
-                            </div>
+                        <input value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} onKeyDown={(e) => e.key === "Enter" && sendMessage()} type="text" placeholder="Send a message" className="w-full py-3 pl-10 text-gray-600 placeholder-gray-600 bg-gray-200 rounded-md focus:outline-none focus:placeholder-gray-400" />
+                        <div class="absolute right-0 items-center inset-y-0 hidden sm:flex">
+                            <button onClick={sendMessage} type="button" className="inline-flex items-center justify-center px-2 py-3 text-white transition duration-500 ease-in-out bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none">
+                                <span className="font-bold">Send</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6 ml-2 transform rotate-90">
+                                    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                                </svg>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>        </>
