0c31c705d09267d598e46dc6dbf1c7d2f32d3707
Danielle Thoreson <danielle_thoreson@ymail.com>
2023-04-28 11:20:03 -0700

    change to SOC Welcome Tab! :D

--- client/src/views/sideViewOneCharacter.jsx
+++ client/src/views/sideViewOneCharacter.jsx
@@ -43,39 +43,52 @@ function Tabs(props) {
           <li className="w-full">
               <a href="#" className="inline-block w-full p-4 bg-white rounded-r-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Invoice</a>
           </li> */}
-        </ul>
-        <br/>
-        <div className="content-tabs ">
-          <div
-            className={ ' ' + (toggleState === 1 ? "content  active-content " : "  hidden")}
-            >
-            <h2 className="mb-3">Inventory Items: </h2>
-            <hr />
+      </ul>
+      <br />
+      <div className="content-tabs ">
+        <div
+          className={toggleState === 0 ? "content rounded-lg active-content bg-black text-white p-5" : "hidden"}
+        >
+          <h2 className="text-center font-bold text-4xl p-10">Welcome to your Character Page!</h2>
+          <figure className="relative max-w-full cursor-pointer filter-grayscale">
+            <a href="#">
+              <img className="transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0 shadow-xl shadow-yellow-900 transition-all duration-300" src="https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/UUID-cl9ie6z2h0595vaoii6e8ks41/public" alt="stealthy treasure mimic"/>
+            </a>
+            <figcaption className="absolute px-4 text-lg text-white bottom-6">
+              <p className="text-center pb-6">...Ready to roll the dice?</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div
+          className={ ' ' + (toggleState === 1 ? "content  active-content " : "  hidden")}
+        >
+          <h2 className="mb-3">Inventory Items: </h2>
+          <hr />
             {/* UNCOMMENT when fixed */}
             {/* <p className="text-right"><button onClick={handleAddItems}>Add items to inventory</button></p> */}
             <ItemDrawer isOpen={isItemListOpen} setIsOpen={setIsItemListOpen}/>
-            <ul>
-              {items.map((item, idx) => 
-              <li key={item._id} className="ml-5">{item.name} {character.inventory[idx].quantity}</li>
-              )}
-            </ul>
-          </div>
-
-          <div
-            className={toggleState === 2 ? "content  active-content" : "hidden"}
-            >
-            <h2>Content 2</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-              voluptatum qui adipisci.
-            </p>
-          </div>
-          
+          <ul>
+            {items.map((item, idx) => 
+            <li key={item._id} className="ml-5">{item.name} {character.inventory[idx].quantity}</li>
+            )}
+          </ul>
         </div>
+
+        <div
+          className={toggleState === 2 ? "content  active-content" : "hidden"}
+        >
+          <h2>Content 2</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            voluptatum qui adipisci.
+          </p>
+        </div>
+
+      </div>
       {/* </div> */}
 
-      
+
     </>
   );
 }
