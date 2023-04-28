3cf3df7b075e39bcb0678af75a175fe098cec347
NassarLaith <Nassarlaith.9@gmail.com>
2023-04-27 22:27:23 -0400

    Hello friend :)

--- client/src/views/sideViewOneCharacter.jsx
+++ client/src/views/sideViewOneCharacter.jsx
@@ -45,21 +45,7 @@ function Tabs() {
             voluptatum qui adipisci.
           </p>
         </div>
-
-        <div
-          className={toggleState === 3 ? "content  active-content" : "hidden"}
-        >
-          <h2>Content 3</h2>
-          <hr />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
-            nostrum rerum laudantium totam unde adipisci incidunt modi alias!
-            Accusamus in quia odit aspernatur provident et ad vel distinctio
-            recusandae totam quidem repudiandae omnis veritatis nostrum
-            laboriosam architecto optio rem, dignissimos voluptatum beatae
-            aperiam voluptatem atque. Beatae rerum dolores sunt.
-          </p>
-        </div>
+        
       </div>
     </div>
   );
